5ea3c2b79a7ecd3978a8d5c497a2e6c851c2cf2c
frozenflame83@gmail.com <Keys@Khoas-Mac-mini.local>
2015-07-26 17:31:44 -0500

    R-prog --Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These scripts calculate the inverse matrix for a new matrix 
+## or retrieve from the cache if it is already calculated
 
-## Write a short comment describing this function
+## This function creates a matrix object that can cache its inverse
+## It is a list containing functions to:
+## 1. Set the value of the matrix
+## 2. Get the value of the matrix
+## 3. Set the value of the inverse matrix
+## 4. Get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  ## x must be a square invertible matrix
+  
+  matrix.inv= NULL
+  
+  set = function(y) {
+    ## "<<-" is used to assign a value to an object in an env
+    ## that is different from the current env
+    x <<- y
+    matrix.inv <<- NULL
+  }
+  
+  get = function() x
+  
+  setinv = function(inverse) matrix.inv <<- inverse 
+  
+  getinv = function() matrix.inv
+  
+  list(set =set, get =get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse matrix 
+## of the matrix created above
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  matrix.inv = x$getinv()
+  
+  ## If the result is already calculated
+  if(!is.null(matrix.inv)){
+    message("retrieving from cache")
+    return(matrix.inv)
+  }
+  
+  new_matrix= x$get()
+  matrix.inv = solve(new_matrix,...)
+  
+  x$setinv(matrix.inv)
+  
+  return(matrix.inv)
 }
